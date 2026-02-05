fe1db062f1d5340072766bafc22da0592fe521c9
Matheus Alves <rdptestuser61@gmail.com>
2026-02-05 01:15:46 -0300

    Update product makefiles for edge40neo

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_manaus.mk
+    $(LOCAL_DIR)/twrp_edge40neo.mk
 
-COMMON_LUNCH_CHOICES :=   twrp_manaus-eng 
+COMMON_LUNCH_CHOICES :=   twrp_edge40neo-eng 
 
