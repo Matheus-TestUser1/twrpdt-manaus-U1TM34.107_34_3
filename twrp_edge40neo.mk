e18f0312c3ac20e7a80da28e4a069555a2abeaa2
Matheus Alves <rdptestuser61@gmail.com>
2026-02-05 01:18:12 -0300

    Update TWRP configuration for edge40neo device

--- twrp_edge40neo.mk
+++ twrp_edge40neo.mk
@@ -6,19 +6,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from manaus device
-$(call inherit-product, device/motorola/manaus/device.mk)
+$(call inherit-product, device/motorola/edge40neo/device.mk)
 
 PRODUCT_DEVICE := manaus
-PRODUCT_NAME := twrp_manaus
+PRODUCT_NAME := twrp_edge40neo
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto edge (2022)
 PRODUCT_MANUFACTURER := motorola
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/motorola/manaus/recovery/root,recovery/root)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/motorola/edge40neo/recovery/root,recovery/root)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="manaus_g_vext-user 12 U1TM34.107-34-3 1c7ca3 release-keys"
+    PRIVATE_BUILD_DESC="edge40neo_g_vext-user 12 U1TM34.107-34-3 1c7ca3 release-keys"
 
-BUILD_FINGERPRINT := motorola/manaus_g_vext/manaus:12/U1TM34.107-34-3/1c7ca3:user/release-keys
+BUILD_FINGERPRINT := motorola/edge40neo_g_vext/edge40neo:12/U1TM34.107-34-3/1c7ca3:user/release-keys
