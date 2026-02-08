8b2b2c65dc1ca94e600abdeb4774c785f4983862
Matheus Alves <99508668+Matheus-TestUser1@users.noreply.github.com>
2026-02-08 14:17:08 -0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,8 @@
-# add_lunch_combo twrp_manaus-eng
+#!/bin/bash
+#
+# vendorsetup.sh for Motorola Edge 40 Neo (manaus)
+#
+
+add_lunch_combo twrp_manaus-eng
+add_lunch_combo twrp_manaus-userdebug
+add_lunch_combo twrp_manaus-user
